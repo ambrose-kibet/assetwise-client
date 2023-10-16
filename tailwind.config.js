478644bb39b9f236f06fabd5eff769773a080613
ambrose-kibet <ambrosekibet36@gmail.com>
2023-10-16 22:13:21 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        title: ['Yeseva One', 'sans-serif'],
+      },
+      fontSize: {},
+    },
+  },
+  plugins: [],
+};
