d993030925589931a6f9adc4d4ea7be904b61989
ambrose-kibet <ambrosekibet36@gmail.com>
2023-11-16 21:45:31 +0300

    Add logout functionality to sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,16 +1,26 @@
-import { FaTimes } from 'react-icons/fa';
+import { FaPowerOff, FaTimes } from 'react-icons/fa';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { RootState } from '../redux/store';
 import { closeSidebar } from '../redux/features/nav/navSlice';
 import PropertiesAcordion from './PropertiesAcordion';
 import { Link } from 'react-router-dom';
 import LogoComponent from './LogoComponent';
+import { logoutUser } from '../redux/features/auth/authSlice';
+import styled from 'styled-components';
 const Sidebar = () => {
   const { isSidebarOpen } = useAppSelector((state: RootState) => state.nav);
   const dispatch = useAppDispatch();
+  const { user } = useAppSelector((state: RootState) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    dispatch(closeSidebar());
+  };
 
   return (
-    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
+    <NavbarContainer
+      className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}
+    >
       <div className="sidebar-header">
         <LogoComponent />
         <button
@@ -35,8 +45,26 @@ const Sidebar = () => {
           Contact Us{' '}
         </Link>
       </div>
-      <div className="sidebar-footer"></div>
-    </aside>
+      <div className="sidebar-footer">
+        {user && (
+          <button type="button" onClick={handleLogout} className="button">
+            Log out
+            <span className="button-icon ">
+              <FaPowerOff className="link-icon" />{' '}
+            </span>
+          </button>
+        )}
+      </div>
+    </NavbarContainer>
   );
 };
 export default Sidebar;
+
+const NavbarContainer = styled.aside`
+  position: fixed;
+  top: 0;
+  left: 0;
+  .sidebar-footer {
+    margin-top: auto;
+  }
+`;
